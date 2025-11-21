8a9972aa557fec4a9be7e8436ea2ed1b644872cd
WesleyReis13 <lopeswesley1392@gmail.com>
2025-11-21 16:00:44 -0300

    feat/schema-weather

--- backend/src/weather/weather.module.ts
+++ backend/src/weather/weather.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { WeatherService } from './weather.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { WeatherController } from './weather.controller';
+import { WeatherService } from './weather.service';
+import { Weather, WeatherSchema } from './schemas/weather.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Weather.name, schema: WeatherSchema }]),
+  ],
+  controllers: [WeatherController],
   providers: [WeatherService],
-  controllers: [WeatherController]
 })
 export class WeatherModule {}
--- backend/src/weather/weather.service.ts
+++ backend/src/weather/weather.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Weather, WeatherDocument } from './schemas/weather.schema';
 
 @Injectable()
-export class WeatherService {}
+export class WeatherService {
+  constructor(
+    @InjectModel(Weather.name) private weatherModel: Model<WeatherDocument>,
+  ) {}
+
+  async create(weatherData: Partial<Weather>): Promise<Weather> {
+    const createdWeather = new this.weatherModel(weatherData);
+    return createdWeather.save();
+  }
+
+  async findAll(): Promise<Weather[]> {
+    return this.weatherModel.find().exec();
+  }
+}
